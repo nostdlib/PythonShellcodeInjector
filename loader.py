c099e5095e0a867d368e779ba88cd5d872b974a5
mrzaxaryan <mrzaxaryan>
2026-04-05 13:09:09 +0400

    Fix exit code handling by converting unsigned 32-bit code to signed for os._exit()

--- loader.py
+++ loader.py
@@ -573,6 +573,9 @@ def main():
         else:
             code = run_mmap(shellcode)
 
+    # Convert unsigned 32-bit exit code to signed for os._exit()
+    if code > 0x7FFFFFFF:
+        code = code - 0x100000000
     _log('ok', "Exit code: %d" % code)
     os._exit(code)
 
